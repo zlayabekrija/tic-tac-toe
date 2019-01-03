3fd46813cca6f5963f7c02880a849652c3c976c8
zlayabekrija <zlaja.bekrija@gmail.com>
2019-01-03 18:04:27 +0000

    refractor

--- bin/cmd_msgs.rb
+++ bin/cmd_msgs.rb
@@ -1,3 +1,41 @@
+# Messages class
 class CmdMsgs
-    
+  def self.player_name
+    i = 0
+    b = []
+    while i < 2
+      puts "Enter the player #{i + 1} name"
+      a = gets.chomp
+      b << a.to_s
+      i += 1
+    end
+    display_names(b)
+  end
+
+  def self.display_names(name)
+    i = 0
+    while i < name.count
+      puts "Player #{i + 1} name is #{name[i]}"
+      i += 1
+    end
+  end
+
+  def self.display(arr)
+    @res = arr
+    puts @grid = "
+      #{@res[0]}|#{@res[1]}|#{@res[2]}
+      -----
+      #{@res[3]}|#{@res[4]}|#{@res[5]}
+      -----
+      #{@res[6]}|#{@res[7]}|#{@res[8]}"
+  end
+
+  def self.choice
+    puts 'Please choose position'
+    @x = gets.chomp
+  end
+  
+  def self.winner(win)
+      puts "#{win} wins"
+  end
 end
--- ttt.rb
+++ ttt.rb
@@ -1,85 +1,41 @@
-require_relative "./bin/cmd_msgs.rb"
-# Assign Player name class
-class Player
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
-
-  def choice
-    puts 'Please choose position'
-    @x = gets.chomp
-  end
-end
-# Class That Starts the Game
-class Game
-  def start
-    puts 'Enter player name'
-    a = gets.chomp
-    Player.new(a)
-  end
-end
-# Class That Displays The Grid
-class Grid
-  def initialize(arr)
-    @res = arr
-    display
-  end
-
-  def display
-    puts @grid = "
-        #{@res[0]}|#{@res[1]}|#{@res[2]}
-        -----
-        #{@res[3]}|#{@res[4]}|#{@res[5]}
-        -----
-        #{@res[6]}|#{@res[7]}|#{@res[8]}
-        "
-  end
-end
+require_relative './bin/cmd_msgs.rb'
 # Class for Acutal GamePlay
 class Turn
-  def initialize(player1, player2)
-    @p1 = player1.start
-    @p2 = player2.start
-    puts "Player1 is #{@p1.name}"
-    puts "Player2 is #{@p2.name}"
-    turns
-  end
-
+  CmdMsgs.player_name
   def turns
     @result = false
     @arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     @i = 0
     until @result
-      Grid.new(@arr)
-      if @i.even?
-        a = @p1.choice.to_i
-        @arr[a - 1] = 'X' if check a
-      else
-        b = @p2.choice.to_i
-        @arr[b - 1] = 'O' if check b
-      end
+      CmdMsgs.display @arr
+      add_sign
       @i += 1
       check_winner @arr, @i
+    end
+  end
+
+  def add_sign
+    if @i.even?
+      a = CmdMsgs.choice.to_i
+      @arr[a - 1] = 'X' if check a
+    else
+      b = CmdMsgs.choice.to_i
+      @arr[b - 1] = 'O' if check b
     end
   end
 
   def check_winner(turn_arr, count_turns)
     wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
             [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    pla = count_turns.even? ? 'X' : 'O'
     i = 0
-    while i < wins.length && @result == false
+    while i < wins.length && !@result
       j = 0
-      while j < wins[i].length && @result == false
-        if turn_arr[wins[i][j]] == 'X' && turn_arr[wins[i][j + 1]] == 'X' &&
-           turn_arr[wins[i][j + 2]] == 'X'
-          @result = true
-          puts 'X wins'
-        elsif turn_arr[wins[i][j]] == 'O' && turn_arr[wins[i][j + 1]] == 'O' &&
-              turn_arr[wins[i][j + 2]] == 'O'
-          @result = true
-          puts 'O wins'
+      while j < wins[i].length && !@result
+        if turn_arr[wins[i][j]] == pla && turn_arr[wins[i][j + 1]] == pla &&
+           turn_arr[wins[i][j + 2]] == pla
+         @result = true
+          CmdMsgs.winner(pla)
         elsif count_turns == 9 && !@result
           @result = true
           puts 'Nobody wins'
@@ -93,12 +49,23 @@ class Turn
   end
 
   def check(mate)
-    unless (1..9).cover?(mate)
-      puts "Don't be clever!!!"
-      @i -= 1
-    end
-    if @arr[mate - 1] == 'X' || @arr[mate - 1] == 'O'
+    return true if check_position(mate) && check_space(mate)
+
+    false
+  end
+
+  def check_space(spac)
+    if @arr[spac - 1] == 'X' || @arr[spac - 1] == 'O'
       puts "Just don't try it"
+      @i -= 1
+      return false
+    end
+    true
+  end
+
+  def check_position(pos)
+    unless (1..9).cover?(pos)
+      puts "Don't be clever!!!"
       @i -= 1
       return false
     end
@@ -107,9 +74,8 @@ class Turn
 end
 
 def game_play
-  a = Game.new
-  b = Game.new
-  c = Turn.new(a, b)
+  c = Turn.new
+  c.turns
   puts 'Game over'
   puts 'Type yes to play another game'
   n = gets.chomp
